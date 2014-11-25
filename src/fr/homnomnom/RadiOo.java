817403f228648672a2d78fc7b14ac4f3b75b760f
TheMrNomis <n0m1s@homnomnom.fr>
2014-11-25 22:46:12 +0100

    change from C++ to java

---
+++ src/fr/homnomnom/RadiOo.java
@@ -0,0 +1,15 @@
+package fr.homnomnom;
+
+import marytts.*;
+
+public class RadiOo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
